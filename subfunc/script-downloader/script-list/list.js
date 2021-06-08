b789929bc9a6c42491cafb1480c4bebe3708bb96
Aditia <aditia.tobby@hreasily.com>
2021-06-08 18:49:42 +0700

    6971387072364023042

---
+++ subfunc/script-downloader/script-list/list.js
@@ -0,0 +1,10 @@
+var list = `{
+	"6971387072364023042": {
+		"link": [
+			{
+				"label":"BIKIN WEB, TOMBOLNYA KABUR",
+				"url":"https://raw.githubusercontent.com/nvmbrxi/nvmbrxi.github.io/nvmbrxi/subfunc/script-downloader/script-list/6971387072364023042.html"
+			}
+		]
+	}
+}`;
